8df88f46a2d4fc677d7e81a4f1d366b5393c49bd
Jason Hooper <jason.hooper@talk21.com>
2013-12-11 22:48:14 +0000

    competition edit, show, update done

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -4,11 +4,31 @@ class CompetitionsController < ApplicationController
     @comp = Competition.new(:user_id => current_user)
   end
 
+  def show
+    @comp = Competition.find(params[:id])
+  end
+
+  def edit
+    @comp = Competition.find(params[:id])
+  end
+
+  def update
+    @comp = Competition.find(params[:id])
+    if @comp.update_attributes(competition_params)
+      flash[:info] = "Competition updated successfully."
+      redirect_to :action => "show", :id => @comp.id
+    else
+      flash[:alert] = "Error updating competition!\n"
+      flash[:alert] << @comp.errors.full_messages.join(".\n")
+      render :edit
+    end
+  end
+
   def create
     @comp = Competition.new(competition_params)
     @comp.user_id = current_user.id
     if @comp.save
-      flash[:message] = "Competition created successfully."
+      flash[:info] = "Competition created successfully."
       redirect_to :action => "show", :id => @comp.id
     else
       flash[:alert] = "Error creating competition!\n"
