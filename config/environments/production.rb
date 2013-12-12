9fdaf1e2a24b41aed55fad4a7109955dca6ffe4e
Jason Hooper <jason.hooper@talk21.com>
2013-12-12 19:48:39 +0000

    email settings for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,12 @@ Competmansys::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  user_name:            ENV['emailid'],
+  password:             ENV['emailpass'],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
 end
