58dbf741442b299da5270533dd4e368241a6b0e2
Jason Hooper <jason.hooper@talk21.com>
2013-12-15 15:34:03 +0000

    destroy event registrations when withdrawing from a competition

--- app/models/competition_attendee.rb
+++ app/models/competition_attendee.rb
@@ -1,5 +1,5 @@
 class CompetitionAttendee < ActiveRecord::Base
   belongs_to :user
   belongs_to :competition
-  has_many :event_registrations
+  has_many :event_registrations, :dependent => :destroy
 end
