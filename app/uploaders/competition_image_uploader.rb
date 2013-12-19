f4c3ed28d8eba0e88609db8456dfe52797407b49
Jason Hooper <jason.hooper@talk21.com>
2013-12-19 11:42:23 +0000

    more styling

--- app/uploaders/competition_image_uploader.rb
+++ app/uploaders/competition_image_uploader.rb
@@ -33,7 +33,7 @@ class CompetitionImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fill => [75, 75]
+    process :resize_to_fill => [100, 100]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
