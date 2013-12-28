75c15a625f59bc5ae7544f37eefc229c10cd0b88
Jason Hooper <jason.hooper@talk21.com>
2013-12-28 20:26:10 +0000

    expand User model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,13 @@ describe User do
   it { should have_many(:competitions) }
   it { should have_many(:competition_administrators) }
   it { should have_many(:competition_attendees) }
+  it { should have_many(:attending).through(:competition_attendees)}
+  it { should have_many(:registrations).through(:competition_attendees)}
+  it { should have_many(:entered).through(:registrations)}
   it { should validate_presence_of(:first_name) }
   it { should validate_presence_of(:last_name) }
+  it "returns first_name and last_name as full_name" do
+    user = User.make!
+    user.full_name.should == user.first_name + ' ' + user.last_name
+  end
 end
